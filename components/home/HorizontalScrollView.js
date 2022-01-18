6aba107f7af563db0a71d43b107b4fcd4d4cf025
Abdullah Ali <abdullahprince7717@gmail.com>
2022-01-19 00:49:01 +0500

    minor changes

--- components/home/HorizontalScrollView.js
+++ components/home/HorizontalScrollView.js
@@ -6,14 +6,14 @@ return(
     
     <View style = {{height: 160, marginTop:90}}>
 
-            <View style = {{height: 160, width:120, marginLeft: 5 }} >
+            <View style = {{height: 160, width:120, marginLeft: 7 }} >
                 <View style = {{flex:2}}>
                     <Image source = {props.image} style = {{flex: 1, width: null, height: null, resizeMode: 'cover', borderRadius: 60}} />
 
                 </View>
 
-                <View style = {{flex:1, paddingTop: 10,  alignItems: 'center' }}>
-                    <Text>{props.name}</Text>
+                <View style = {{flex:1, paddingTop: 10,  alignItems: 'center',   }}>
+                    <Text style={{color: 'white'}}>{props.name}</Text>
                 </View>
 
             </View>
