c75cb6e10de914ff6b4fc0ed1a30db19d7cff512
Abdullah Ali <abdullahprince7717@gmail.com>
2022-04-25 11:51:20 +0500

    finished mapscreen UI

---
+++ Navigation/NewBusinessUserStack.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const NewBusinessUserStack = () => {
+  return (
+    <View>
+      <Text>NewBusinessUserStack</Text>
+    </View>
+  )
+}
+
+export default NewBusinessUserStack
+
+const styles = StyleSheet.create({})
